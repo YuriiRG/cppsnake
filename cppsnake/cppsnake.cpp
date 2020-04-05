0e756ad51cc862f8ed086d482d4e3351bac19500
Yurii Romashko-Gomoliako <yuriirg@tutanota.com>
2020-04-06 02:06:46 +0300

    Copy-paste from paste4btc.com/Lu9Cvpd9, and add comments

--- cppsnake/cppsnake.cpp
+++ cppsnake/cppsnake.cpp
@@ -1,61 +1,67 @@
-﻿#include <iostream>
-#include <conio.h>
-#include <windows.h> // Sleep()
+﻿#include <iostream> // cout, endl
+#include <conio.h> // getch, kbhit
+#include <windows.h> // Sleep
 
 using namespace std;
 
-bool gameOver;
-const int WIDTH = 20;
-const int HEIGHT = 20;
+bool gameOver; // global variables
+const int width = 20;
+const int height = 20;
 int x, y, fruitX, fruitY, score;
-enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
-eDirection dir;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirecton dir;
 
-void setup() {
+void Setup() { // set default values
     gameOver = false;
     dir = STOP;
-    x = WIDTH / 2 - 1;
-    y = HEIGHT / 2 - 1;
-    fruitX = rand() % WIDTH;
-    fruitY = rand() % HEIGHT;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
     score = 0;
 }
-
-void draw() {
+void Draw() {
     system("cls");
-    for (int i = 0; i <= WIDTH; i++) {
-        cout << "#";
-    }
+    for (int i = 0; i < width + 2; i++)
+        cout << "#"; // top wall
     cout << endl;
 
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-            if ((j == 0) || (j == (WIDTH - 1))) {
-                cout << "#";
-            }
-            if ((i == y) && (j == x)) {
-                cout << "0";
-            }
-            else if ((i == fruitY) && (j == fruitX)) {
-                cout << "F"; // press F to pay respects
-            }
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0)
+                cout << "#"; // left wall
+            if (i == y && j == x) // snake head
+                cout << "O";
+            else if (i == fruitY && j == fruitX) // fruit
+                cout << "F";
             else {
-                cout << " ";
+                bool print = false;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        cout << "o"; // snake tail
+                        print = true;
+                    }
+                }
+                if (!print)
+                    cout << " "; //  field
             }
+
+            if (j == width - 1)
+                cout << "#"; // right wall
         }
         cout << endl;
     }
 
-    for (int i = 0; i <= WIDTH; i++) {
-        cout << "#";
-    }
+    for (int i = 0; i < width + 2; i++) 
+        cout << "#"; // bottom wall
     cout << endl;
-    cout << "Score: " << score;
+    cout << "Score:" << score << endl;
 }
-
-void input() {
-    if (_kbhit()) {
-        switch (_getch()) {
+void Input() {
+    if (_kbhit()) { // if button pressed
+        switch (_getch()) { // get this button value
         case 'a':
             dir = LEFT;
             break;
@@ -74,10 +80,21 @@ void input() {
         }
     }
 }
-
-void logic() {
-    switch (dir)
-    {
+void Logic() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y; // variables to change tail
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++) { // change tail
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+    switch (dir) { // change position
     case LEFT:
         x--;
         break;
@@ -91,23 +108,34 @@ void logic() {
         y++;
         break;
     }
-    if ((x > WIDTH) || (x < 0) || (y > HEIGHT) || (y < 0)) {
-        gameOver = true;
-    }
-    if ((x == fruitX) && (y == fruitY)) {
-        score++;
-        fruitX = rand() % WIDTH;
-        fruitY = rand() % HEIGHT;
+
+    if (x >= width) // go through walls
+        x = 0;
+    else if(x < 0)
+        x = width - 1;
+    if (y >= height)
+        y = 0;
+    else if (y < 0)
+        y = height - 1;
+
+    for (int i = 0; i < nTail; i++) // if touch tail
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    if (x == fruitX && y == fruitY) { // if get fruit
+        score += 1;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
     }
 }
-
 int main() {
-    setup();
-    while (!gameOver) {
-        draw();
-        input();
-        logic();
-        Sleep(20); // To be not too fast
+    Setup();
+    while (!gameOver) { // main loop
+        Draw();
+        Input();
+        Logic();
+        Sleep(20); // to be not too fast
     }
     return 0;
 }
